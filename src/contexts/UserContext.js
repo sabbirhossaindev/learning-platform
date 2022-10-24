fff40450e87f60a3292ef87aa03531d642893239
sabbir hossain <mdsabbir477470@gmail.com>
2022-10-24 23:16:05 +0600

    some file added

---
+++ src/contexts/UserContext.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserContext = () => {
+    return (
+        <div>
+            <h2>This is UserContext compo</h2>
+        </div>
+    );
+};
+
+export default UserContext;
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC_2z5_ZtHun0U7RV3zv5Z5wh9QdySQ4NU",
+  authDomain: "learning-platform-client-9c054.firebaseapp.com",
+  projectId: "learning-platform-client-9c054",
+  storageBucket: "learning-platform-client-9c054.appspot.com",
+  messagingSenderId: "1039293507772",
+  appId: "1:1039293507772:web:0ddd06451cfeffc67908b9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
