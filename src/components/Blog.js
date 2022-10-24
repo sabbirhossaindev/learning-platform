57f1363e8a07d11a569d1583fd95358236e7cc01
sabbir hossain <mdsabbir477470@gmail.com>
2022-10-25 01:26:51 +0600

    Blog component done

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -7,10 +7,10 @@ const Blog = () => {
             <div className='p-5'>
                 <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
                 <div className="collapse-title text-xl font-medium">
-                    Focus me to see content
+                    what is cors?
                 </div>
                 <div className="collapse-content"> 
-                    <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+                    <p>CORS is shorthand for Cross-Origin Resource Sharing. It is a mechanism to allow or restrict requested resources on a web server depend on where the HTTP request was initiated. This policy is used to secure a certain web server from access by other website or domain.</p>
                 </div>
                 </div>
             </div>
@@ -18,10 +18,13 @@ const Blog = () => {
             <div className='p-5'>
                 <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
                 <div className="collapse-title text-xl font-medium">
-                    Focus me to see content
+                    Why are you using firebase? <br />
+                    What other options do you have to implement authentication?
                 </div>
                 <div className="collapse-content"> 
-                    <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+                    <p>Thanks to Firebase's easy-to-use SDK, ready-made UI libraries, and backend services, it is possible to set up the whole authentication process in just a couple of minutes. Firebase allows users to authenticate with their phone number, email, Google, and Facebook accounts (more options can be found here <br /> <br />
+                    
+                    Usually, authentication by a server entails the use of a user name and password. Other ways to authenticate can be through cards, retina scans, voice recognition, and fingerprints.</p>    
                 </div>
                 </div>
             </div>
@@ -29,10 +32,10 @@ const Blog = () => {
             <div className='p-5'>
                 <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
                 <div className="collapse-title text-xl font-medium">
-                    Focus me to see content
+                    How does the private route work?
                 </div>
                 <div className="collapse-content"> 
-                    <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+                    <p>The react private route component renders child components ( children ) if the user is logged in. If not logged in the user is redirected to the /login page with the return url passed in the location state property.</p>
                 </div>
                 </div>
             </div>
@@ -40,10 +43,12 @@ const Blog = () => {
             <div className='p-5'>
                 <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
                 <div className="collapse-title text-xl font-medium">
-                    Focus me to see content
+                    What is Node? How does Node work?
                 </div>
                 <div className="collapse-content"> 
-                    <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+                    <p>Per the Node.js homepage, Node “uses an event-driven, non-blocking I/O model.” In practice, this means that Node is built well to handle asynchronous JavaScript code to perform many asynchronous activities such as reading and writing to the file system, handling connections to database servers, or handling requests as a web server. <br /> <br />
+
+                    It is a used as backend service where javascript works on the server-side of the application. This way javascript is used on both frontend and backend. Node. js runs on chrome v8 engine which converts javascript code into machine code, it is highly scalable, lightweight, fast, and data-intensive.</p>
                 </div>
                 </div>
             </div>
