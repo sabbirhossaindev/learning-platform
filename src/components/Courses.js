f8f8e4c8250464c862adaca04670ebb74176cbbc
sabbir hossain <mdsabbir477470@gmail.com>
2022-10-25 10:03:29 +0600

    course design done

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -2,8 +2,46 @@ import React from 'react';
 
 const Courses = () => {
     return (
-        <div>
-            <h2>this is Courses compo</h2>
+        <div className='flex justify-center items-center min-h-screen bg-indigo-500'>
+            <div className='flex-1 max-w-5xl p-16'>
+                <div className='grid grid-cols-1 grid-rows-4 gap-4 grid-flow-row-dense lg:grid-cols-3'>
+                    <div className='p-4 pr-6 bg-white border-1-8 border-transparent rounded-md shadow-md space-y-2'>
+                        <h2 className='text-lg font-semibold leading-6'>Home renovations</h2>
+                        <p className='text-gray-600'>Lorem ipsum dolor sit amet.</p>
+                    </div>
+
+                    <div className='p-4 pr-6 bg-white border-1-8 border-transparent rounded-md shadow-md space-y-2'>
+                        <h2 className='text-lg font-semibold leading-6'>Home renovations</h2>
+                        <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur</p>
+                    </div>
+
+                    <div className='row-span-full p-4 bg-white border-1-8 border-transparent rounded-md shadow-md space-y-2'>
+                        <h2 className='text-lg font-semibold leading-6'>Home renovations</h2>
+                        <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum soluta dignissimos eveniet minima labore deleniti impedit fuga facere optio repellendus.</p>
+                    </div>
+
+                    <div className='p-4 pr-6 bg-white border-1-8 border-transparent rounded-md shadow-md space-y-2'>
+                        <h2 className='text-lg font-semibold leading-6'>Home renovations</h2>
+                        <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur</p>
+                    </div>
+
+                    <div className='p-4 pr-6 bg-white border-1-8 border-transparent rounded-md shadow-md space-y-2'>
+                        <h2 className='text-lg font-semibold leading-6'>Home renovations</h2>
+                        <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur</p>
+                    </div>
+
+                    <div className='p-4 pr-6 bg-white border-1-8 border-transparent rounded-md shadow-md space-y-2'>
+                        <h2 className='text-lg font-semibold leading-6'>Home renovations</h2>
+                        <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur</p>
+                    </div>
+
+                    <div className='p-4 pr-6 bg-white border-1-8 border-transparent rounded-md shadow-md space-y-2'>
+                        <h2 className='text-lg font-semibold leading-6'>Home renovations</h2>
+                        <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur</p>
+                    </div>
+                    
+                </div>
+            </div>
         </div>
     );
 };
