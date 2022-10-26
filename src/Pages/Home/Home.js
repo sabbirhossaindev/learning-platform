7aebd3f15685875ddb72312a7e9c1e15a8fb3960
sabbir hossain <mdsabbir477470@gmail.com>
2022-10-26 12:50:53 +0600

    home component done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import banner from '../../assets/banner.gif'
+import './Home.css';
+import Button from 'react-bootstrap/Button';
 
 const Home = () => {
     return (
-        <div>
-            <h2>This is home</h2>
-        </div>
+        <Container className='text-center p-4'>
+            <Row>
+                <Col lg="6">
+                    <h2 className='text-primary'>Hey Hello viewers, <br /> Welcome to my Awesome Courses</h2>
+                    <p className='text-secondary'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit quia magni commodi at voluptates rem, officia sequi voluptatum ab, doloribus suscipit possimus doloremque. Possimus adipisci voluptatum saepe quisquam similique vero, nulla unde quas placeat aut odit temporibus magni odio doloremque!</p>
+                    <Button variant="primary">Please Login</Button>{' '}
+                </Col>
+
+                <Col lg="6">
+                    <img src={banner} alt="" className='mt-4 banner' />
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
