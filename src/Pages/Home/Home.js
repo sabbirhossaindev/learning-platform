64d1b50c545f641e16fc11926f67c53abeb5da13
sabbir hossain <mdsabbir477470@gmail.com>
2022-10-27 22:55:40 +0600

    Allahhandull all work done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
                 <Col lg="6">
                     <h2 className='text-primary'>Hey Hello viewers, <br /> Welcome to my Awesome Courses</h2>
                     <p className='text-secondary'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit quia magni commodi at voluptates rem, officia sequi voluptatum ab, doloribus suscipit possimus doloremque. Possimus adipisci voluptatum saepe quisquam similique vero, nulla unde quas placeat aut odit temporibus magni odio doloremque!</p>
-                    <Link to='/login' className='link'><Button variant="outline-primary">Please Login</Button></Link>
+                    <Link className='link'><Button variant="outline-primary">Welcome</Button></Link>
                 </Col>
 
                 <Col lg="6">
